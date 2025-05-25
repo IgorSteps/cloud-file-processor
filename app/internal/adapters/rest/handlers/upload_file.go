58f3437e18f445c15132b0a4964e755dbba3bde7
Igor Stepanenko <igorstepanenko696@gmail.com>
2025-05-25 11:10:55 +0000

    sp

--- app/internal/adapters/rest/handlers/upload_file.go
+++ app/internal/adapters/rest/handlers/upload_file.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"app/internal/adapters/rest/models"
+	"app/internal/domain/entities"
+	"context"
 	"encoding/json"
 	"log/slog"
 	"net/http"
@@ -9,6 +11,11 @@ import (
 
 type UploadFile struct {
 	backupLogger *slog.Logger
+	fileUploader FileUploader
+}
+
+type FileUploader interface {
+	Execute(ctx context.Context, file *entities.File) (string, error)
 }
 
 func (s *UploadFile) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -25,4 +32,26 @@ func (s *UploadFile) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	domainFile, err := file.ToDomain(r.Context(), s.backupLogger)
+	if err != nil {
+		s.backupLogger.ErrorContext(r.Context(),
+			"failed to convert file upload request to domain",
+			"error", err,
+		)
+		http.Error(w, "request body is invalid", http.StatusBadRequest)
+		return
+	}
+
+	uri, err := s.fileUploader.Execute(r.Context(), domainFile)
+	if err != nil {
+		switch err.(type) {
+		default:
+			s.backupLogger.ErrorContext(r.Context(), "unknown error while uploading file", "error", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(uri)
 }
